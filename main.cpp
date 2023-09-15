cb290836f9da0c682887901cd1a70b33c50d1750
TechieAbhijeet <k12.abhijeet@gmail.com>
2023-09-15 15:55:47 +0530

    new file added

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// One line comment 
+
+//This brings in the iostream library
+ #include <iostream>
+
+/*
+This is a block comment
+*/
+int main (){
+    //This is going to print "Hello world" to the consle
+  std::cout << "Hello world!" << std::endl; 
+return 0;
+// Program ends here
+}
